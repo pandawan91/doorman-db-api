892bcc7ffc38e1058cce99d7b67b7e8c818ecaca
Vuong Ngo <v.ngo@cpc-baulogistik.de>
2017-10-08 21:40:59 +0200

    add business layer

---
+++ doorman-db-api/doorman-db-api-service/implementations/CardService.cs
@@ -0,0 +1,41 @@
+﻿namespace doorman_db_api.service.implementations
+{
+    using data.implementations;
+    using data.implementations.entities;
+    using data.interfaces;
+    using interfaces;
+    using System;
+    using System.Linq;
+
+    public class CardService : ICardService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CardService(IContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+            _unitOfWork = new UnitOfWork(context);
+        }
+
+        public void AddCardIdIfNotExists(int cardId)
+        {
+            if (cardId < 0)
+                throw new ArgumentOutOfRangeException(nameof(cardId));
+            if(!_unitOfWork.CardRepository.Get.Any(x => x.CardId == cardId))
+            {
+                _unitOfWork.CardRepository.Add(new CardEntity()
+                {
+                    CardId = cardId
+                });
+            }
+        }
+
+        public bool IsCardIdInDataBase(int cardId)
+        {
+            if (cardId < 0)
+                throw new ArgumentOutOfRangeException(nameof(cardId));
+            return _unitOfWork.CardRepository.Get.Any(x => x.CardId == cardId);
+        }
+    }
+}
